98eb690947ade1d3d00b9b21f8419df5700fe779
Nathan Hamblen <nathan@technically.us>
2014-05-26 17:44:55 -0400

    some changes to track objects defined in other compilation modules better

--- porthole/src/main/scala/porthole.scala
+++ porthole/src/main/scala/porthole.scala
@@ -5,23 +5,29 @@ import language.experimental.macros
 import reflect.macros.Context
 
 object Porthole {
-  def apply(param: Any): List[String] = macro applyImpl
+  def apply(param: Any): (String, List[String]) = macro applyImpl
 
-  def applyImpl(c: Context)(param: c.Expr[Any]): c.Expr[List[String]] = {
+  def applyImpl(c: Context)(param: c.Expr[Any]): c.Expr[(String,List[String])] = {
     import c.universe._
-    val (vparams, body) = param.tree match {
-      case Function(vparams, body) => (vparams, body)
-      case Block(_, Function(vparams, body)) => (vparams, body)
-      case _ => c.abort(c.enclosingPosition, 
+    def mismatch() =
+      c.abort(c.enclosingPosition,
         "Porthole mismatch:\nMust be called directly on a method, e.g. Porhole(MyObject.myMethod _)")
+
+    val body = param.tree match {
+      case Function(vparams, body) => body
+      case Block(_, Function(vparams, body)) => body
+      case _ => mismatch()
     }
-    val funcName = body match {
-      case Apply(s) => s
+    val (methodName, params) = body match {
+      case Apply(Select(obj, method), params) => (obj.toString + "." + method.decoded, params)
+      case _ => mismatch()
     }
     val listApply = Select(reify(List).tree, newTermName("apply"))
+    val names = params.map {
+      case Ident(p) => c.literal(p.decoded).tree
+    }
 
-    val names = vparams.map( p => c.literal(p.name.decoded).tree)
-
-    c.Expr[List[String]](Apply(listApply, names))
+    val argList = c.Expr[List[String]](Apply(listApply, names))
+    reify(c.literal(methodName).splice -> argList.splice)
   }
 }
